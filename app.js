423869dee1fb8005fc3535d5ced6353f45c761b5
kittenmasque <volaju@gmail.com>
2022-07-31 12:54:05 -0700

    added app.get('/login', ...) to app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
+import Login from '../project5_frontend/src/userContainer/loginComponent/loginComponent';
 const { urlencoded } = require('express');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
+const User = require('./models/users')
 const morgan = require('morgan')
 const app = express();
 const session = require('express-session');
+const isLoggedIn = require('../middleware/isLoggedIn')
 const itemController = require("./controllers/itemController")
-const usersController = require("./controllers/usersController")
+const usersController = require("./controllers/usersController");
+const { default: Login } = require('../project5_frontend/src/userContainer/loginComponent/loginComponent');
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/karolin_mongoose_store'
 
@@ -30,8 +34,14 @@ app.use(session({
     saveUninitialized: false,
 }));
 
+
 app.use('/users', usersController)
-app.use('/items', itemController);
+app.use('/items', isLoggedIn, itemController);
+app.get('/login', (req, res) => {
+    res.render(props.Login)
+})
+
+
 const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log('app is running')
--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, unique: true, required: true, minlength: 2},
+    username: {type: String, unique: true, required: true, minlength: 3},
     password: {type: String, unique: false, required: true},
     bikes: [{type: Schema.Types.ObjectId, ref: 'Item'}]
 }, {timestamps: true})
